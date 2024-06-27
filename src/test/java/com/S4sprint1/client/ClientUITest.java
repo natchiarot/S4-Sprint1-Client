fcab4e468c60ee1839ee91c7c449df71dc92de13
sub-zero-cool <138683597+mitchell-barkley@users.noreply.github.com>
2024-06-27 10:33:26 -0230

    Updated client UI, added unit tests

--- src/test/java/com/S4sprint1/client/ClientUITest.java
+++ src/test/java/com/S4sprint1/client/ClientUITest.java
@@ -5,6 +5,7 @@ import org.apache.http.StatusLine;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.util.EntityUtils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -38,47 +39,97 @@ public class ClientUITest {
     }
 
     @Test
+    public void testSetCommand() {
+        clientUI.setCommand("get_city-1");
+        assertEquals("get_city-1", clientUI.command);
+    }
+
+    @Test
+    public void testSetCommandEmpty() {
+        clientUI.setCommand("");
+        assertEquals("", clientUI.command);
+    }
+
+    @Test
     public void testGetRequest() throws IOException {
-        String url = "http://localhost:8080/city/1";
-        String expectedResponse = "St. John's";
+        String url = "http://localhost:8080/";
         when(httpClient.execute(any(HttpGet.class))).thenReturn(httpResponse);
         when(httpResponse.getEntity()).thenReturn(httpEntity);
+        when(httpEntity.getContent()).thenReturn(null);
         when(httpResponse.getStatusLine()).thenReturn(statusLine);
-        when(statusLine.getStatusCode()).thenReturn(200);
-        String response = getRequest(url);
-        assertEquals(expectedResponse, response);
+        when(statusLine.getStatusCode()).thenReturn(404);
+        String response = getRequest(url).split(",")[1].trim();
+
+        assertEquals("\"status\":404", response);
     }
 
     @Test
     public void testGetRequestIOException() throws IOException {
         String url = "http://localhost:8080/";
-        String expectedResponse = null;
         when(httpClient.execute(any(HttpGet.class))).thenThrow(new IOException());
-        String response = getRequest(url);
-        assertEquals(expectedResponse, response);
+        String response = getRequest(url).split(",")[1].trim();
+
+        assertEquals("\"status\":404", response);
     }
 
     @Test
     public void testRun() {
-        clientUI.command = "get airport";
+        clientUI.command = "get_city-1";
         clientUI.run();
+        assertNull(clientUI.response);
     }
 
     @Test
     public void testRunGetCity() {
-        clientUI.command = "get city";
+        clientUI.command = "get_city-1";
         clientUI.run();
     }
 
     @Test
     public void testRunGetCities() {
-        clientUI.command = "get cities";
+        clientUI.command = "get_cities";
         clientUI.run();
     }
 
     @Test
     public void testRunGetAirports() {
-        clientUI.command = "get airports";
+        clientUI.command = "get_airports";
+        clientUI.run();
+    }
+
+    @Test
+    public void testRunGetAirport() {
+        clientUI.command = "get_airport-1";
+        clientUI.run();
+    }
+
+    @Test
+    public void testRunGetFlight() {
+        clientUI.command = "get_flight-1";
+        clientUI.run();
+    }
+
+    @Test
+    public void testRunGetFlights() {
+        clientUI.command = "get_flights";
+        clientUI.run();
+    }
+
+    @Test
+    public void testRunGetPassenger() {
+        clientUI.command = "get_passenger-1";
+        clientUI.run();
+    }
+
+    @Test
+    public void testRunGetPassengers() {
+        clientUI.command = "get_passengers";
+        clientUI.run();
+    }
+
+    @Test
+    public void testRunInvalidCommand() {
+        clientUI.command = "invalid";
         clientUI.run();
     }
 }
